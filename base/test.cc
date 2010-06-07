a13bd1f2c101c23dee6daa361aa03b6681058f3d
shumash <shumash@gmail.com>
2010-06-07 09:04:59 -0400

    base subdire subdir change

--- base/test.cc
+++ base/test.cc
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+namespace first_n {
+
 // This is my first class!
 class MyClass {
-  public:
-   MyClass(int i) : i_(i) {}
-  private:
-   int i_;
+public:
+  MyClass() : internal_variable_(0) {}
+
+  MyClass(int i) : internal_variable_(i) {}
+
+  void print() const {
+    cout << "i = " << internal_variable_;
+  }
+
+private:
+  int internal_variable_;
 };
+
+}  // namespace first_n
